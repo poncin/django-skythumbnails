f8eb3aebbe3e1e41c7b81df0e004668c867ff8a8
Frederic Poncin <frederic.poncin@gmail.com>
2014-02-28 21:45:32 -0800

    remove strict dependency on 1.7.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=".".join(map(str, sky_thumbnails.VERSION)),
     packages = find_packages(),
     install_requires=[
-        'PILLOW==1.7.7'
+        'Pillow'
     ],
 
     author = 'Concentric Sky',
